8b4950e50ad64abcbdc8aa7fdb6d0d5ac9082217
Remmight <91031667+Remmight@users.noreply.github.com>
2021-10-24 19:06:55 +0100

    Update PP.js

--- PP.js
+++ PP.js
@@ -1,6 +1,5 @@
 function dot_product(vector1, vector2) {
   let psResult = 0;
-  let finalResult;
   for (let i = 0; i < 2; i++) {
     psResult += vector1[i] * vector2[i];
   }
@@ -8,13 +7,14 @@ function dot_product(vector1, vector2) {
   return psResult;
 }
 
-function checkOrthagonal(vector1, vector2) {
-  finalResult = dot_product(vector1, vector2);
-  if (finalResult == 0) {
-    console.log("The vectors are perpendicular");
+function checkOrthogonal(vector1, vector2) {
+  result = dot_product(vector1, vector2);
+  if (result == 0) {
+    console.log("The vectors are orthogonal");
   } else {
-    console.log("the dot product is" + " ", finalResult);
+    console.log("the dot product is" + result);
   }
 }
 
-checkOrthagonal([2, 1], [-4, 6]);
+checkOrthagonal([2, 1], [4, 6]);
+checkOrthogonal([3, 0], [0, 6]);
